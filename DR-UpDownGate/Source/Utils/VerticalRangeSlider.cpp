b8afd96bbb303383fcc9dc4b2b5ef6fb67065955
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2025-10-29 03:21:52 -0400

    Implement range slider

--- DR-UpDownGate/Source/Utils/VerticalRangeSlider.cpp
+++ DR-UpDownGate/Source/Utils/VerticalRangeSlider.cpp
@@ -1,5 +1,83 @@
-//
-// Created by esther on 10/29/25.
-//
-
 #include "VerticalRangeSlider.h"
+
+VerticalRangeSlider::VerticalRangeSlider(float min, float max)
+    : minValue(min), maxValue(max), lowerValue(min), upperValue(max)
+{
+    setMouseCursor(juce::MouseCursor::PointingHandCursor);
+}
+
+void VerticalRangeSlider::setLowerValue(float value)
+{
+    lowerValue = juce::jlimit(minValue, upperValue, value);
+    repaint();
+}
+
+void VerticalRangeSlider::setUpperValue(float value)
+{
+    upperValue = juce::jlimit(lowerValue, maxValue, value);
+    repaint();
+}
+
+void VerticalRangeSlider::paint(juce::Graphics& g)
+{
+    auto bounds = getLocalBounds();
+    int sliderX = bounds.getCentreX();
+
+    // Draw track
+    g.setColour(juce::Colours::darkgrey);
+    g.drawLine(sliderX, valueToY(minValue), sliderX, valueToY(maxValue), 4.0f);
+
+    // Highlighted range
+    g.setColour(juce::Colours::skyblue);
+    g.drawLine(sliderX, valueToY(upperValue), sliderX, valueToY(lowerValue), 8.0f);
+
+    // Draw thumbs
+    g.setColour(juce::Colours::white);
+    g.fillEllipse(sliderX - thumbRadius, valueToY(lowerValue) - thumbRadius,
+                  thumbRadius * 2, thumbRadius * 2);
+
+    g.fillEllipse(sliderX - thumbRadius, valueToY(upperValue) - thumbRadius,
+                  thumbRadius * 2, thumbRadius * 2);
+}
+
+void VerticalRangeSlider::resized() {}
+
+int VerticalRangeSlider::valueToY(float value) const
+{
+    // Map value to Y in component
+    auto bounds = getLocalBounds();
+    float proportion = (value - minValue) / (maxValue - minValue);
+    return juce::jmap(1.0f - proportion, float(bounds.getY()), float(bounds.getBottom()));
+}
+
+float VerticalRangeSlider::yToValue(int y) const
+{
+    auto bounds = getLocalBounds();
+    float proportion = 1.0f - ((float)(y - bounds.getY()) / bounds.getHeight());
+    return juce::jlimit(minValue, maxValue, minValue + proportion * (maxValue - minValue));
+}
+
+void VerticalRangeSlider::mouseDown(const juce::MouseEvent& e)
+{
+    int mouseY = e.getPosition().getY();
+    int lowerY = valueToY(lowerValue);
+    int upperY = valueToY(upperValue);
+
+    if (std::abs(mouseY - lowerY) < thumbRadius * 2)
+        dragging = Lower;
+    else if (std::abs(mouseY - upperY) < thumbRadius * 2)
+        dragging = Upper;
+    else
+        dragging = None;
+}
+
+void VerticalRangeSlider::mouseDrag(const juce::MouseEvent& e)
+{
+    int mouseY = e.getPosition().getY();
+    float value = yToValue(mouseY);
+
+    if (dragging == Lower)
+        setLowerValue(value);
+    else if (dragging == Upper)
+        setUpperValue(value);
+}
--- DR-UpDownGate/Source/Utils/VerticalRangeSlider.h
+++ DR-UpDownGate/Source/Utils/VerticalRangeSlider.h
@@ -1,8 +1,40 @@
-//
-// Created by esther on 10/29/25.
-//
+#pragma once
 
-#ifndef DR_UPDOWNGATE_VERTICALRANGESLIDER_H
-#define DR_UPDOWNGATE_VERTICALRANGESLIDER_H
+#include <juce_gui_basics/juce_gui_basics.h>
 
-#endif //DR_UPDOWNGATE_VERTICALRANGESLIDER_H
+class VerticalRangeSlider : public juce::Component
+{
+public:
+    VerticalRangeSlider(float min, float max);
+    ~VerticalRangeSlider() override = default;
+
+    float getLowerValue() const { return lowerValue; }
+    float getUpperValue() const { return upperValue; }
+
+    void setLowerValue(float value);
+    void setUpperValue(float value);
+
+    // Optionally add: onChange callback or listener
+
+    void paint(juce::Graphics&) override;
+    void resized() override;
+
+protected:
+    void mouseDown(const juce::MouseEvent&) override;
+    void mouseDrag(const juce::MouseEvent&) override;
+
+private:
+    float minValue, maxValue;
+    float lowerValue, upperValue;
+
+    enum DraggingThumb { None, Lower, Upper };
+    DraggingThumb dragging = None;
+
+    int thumbRadius = 8;
+
+    // Maps a value to a Y position and vice versa
+    int valueToY(float value) const;
+    float yToValue(int y) const;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(VerticalRangeSlider)
+};
