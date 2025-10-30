8c88eeae390c3e2b44d2a248362d8c67eb432f14
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2025-10-30 13:12:20 -0400

    Fix knob creation in paint (duh)

--- DR-ArpRand/Source/PluginEditor.cpp
+++ DR-ArpRand/Source/PluginEditor.cpp
@@ -2,24 +2,14 @@
 #include "PluginEditor.h"
 
 //==============================================================================
-AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor& p)
-    : AudioProcessorEditor (&p), processorRef (p)
+AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor& processor)
+    : AudioProcessorEditor (&processor), processorRef (processor)
 {
     juce::ignoreUnused (processorRef);
+
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 300);
-}
-
-AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
-{
-}
-
-//==============================================================================
-void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
-{
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
     arpRateKnob = std::make_unique<juce::Slider>(juce::Slider::RotaryVerticalDrag, juce::Slider::TextBoxBelow);
     arpRateKnob->setTextValueSuffix(" Rate");
@@ -30,6 +20,17 @@ void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
         processorRef.parameters, "arpRate", *arpRateKnob);
 
     addAndMakeVisible(*arpRateKnob);
+}
+
+AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void AudioPluginAudioProcessorEditor::paint (juce::Graphics& graphics)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    graphics.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
     //g.setColour (juce::Colours::white);
     //g.setFont (15.0f);
