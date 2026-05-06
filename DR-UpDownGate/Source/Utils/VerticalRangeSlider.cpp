659ed04f13db00d7a43c241e587e25e5251ca900
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2026-05-06 11:03:24 -0400

    Kinda fix slider graphics

--- DR-UpDownGate/Source/Utils/VerticalRangeSlider.cpp
+++ DR-UpDownGate/Source/Utils/VerticalRangeSlider.cpp
@@ -1,142 +1,247 @@
 #include "VerticalRangeSlider.h"
 #include "Theme.h"
 
-// TODO: Slider UI is disconnected from thresholdLow & thresholdHigh
-
-VerticalRangeSlider::VerticalRangeSlider(float min, float max)
-    : minValue(min), maxValue(max), lowerValue(min), upperValue(max)
+VerticalRangeSlider::VerticalRangeSlider(float minimumValue, float maximumValue)
+    : minValue(minimumValue),
+      maxValue(maximumValue),
+      lowerValue(minimumValue),
+      upperValue(maximumValue)
 {
 }
 
-void VerticalRangeSlider::setLowerValue(float value)
+void VerticalRangeSlider::setLowerValue(float newValue)
 {
-    float clamped = juce::jlimit(minValue, upperValue, value);
+    float maximumLowerValue = upperValue - minimumRange;
+    float clampedValue = juce::jlimit(minValue, maximumLowerValue, newValue);
 
-    if (lowerValue != clamped)
+    if (lowerValue != clampedValue)
     {
-        lowerValue = clamped;
+        lowerValue = clampedValue;
 
         if (OnLowerValueChanged)
+        {
             OnLowerValueChanged(lowerValue);
+        }
 
         repaint();
     }
 }
 
-void VerticalRangeSlider::setUpperValue(float value)
+void VerticalRangeSlider::setUpperValue(float newValue)
 {
-    float clamped = juce::jlimit(lowerValue, maxValue, value);
+    float minimumUpperValue = lowerValue + minimumRange;
+    float clampedValue = juce::jlimit(minimumUpperValue, maxValue, newValue);
 
-    if (upperValue != clamped)
+    if (upperValue != clampedValue)
     {
-        upperValue = clamped;
+        upperValue = clampedValue;
 
         if (OnUpperValueChanged)
+        {
             OnUpperValueChanged(upperValue);
+        }
 
         repaint();
     }
 }
 
-void VerticalRangeSlider::setRoundness(float radius)
+void VerticalRangeSlider::setRoundness(float newRadius)
 {
-    roundness = radius;
+    roundness = newRadius;
     repaint();
+}
+
+void VerticalRangeSlider::setMinimumRange(float newMinimumRange)
+{
+    minimumRange = juce::jmax(0.0f, newMinimumRange);
+
+    if ((upperValue - lowerValue) < minimumRange)
+    {
+        upperValue = juce::jmin(maxValue, lowerValue + minimumRange);
+
+        if ((upperValue - lowerValue) < minimumRange)
+        {
+            lowerValue = juce::jmax(minValue, upperValue - minimumRange);
+        }
+    }
+
+    repaint();
+}
+
+juce::Rectangle<float> VerticalRangeSlider::getRangeRectangle() const
+{
+    juce::Rectangle<float> bounds = getLocalBounds().toFloat();
+
+    float upperYPosition = static_cast<float>(valueToY(upperValue));
+    float lowerYPosition = static_cast<float>(valueToY(lowerValue));
+    float rangeHeight = juce::jmax(1.0f, lowerYPosition - upperYPosition);
+
+    return juce::Rectangle<float>(
+        bounds.getX(),
+        upperYPosition,
+        bounds.getWidth(),
+        rangeHeight
+    );
+}
+
+juce::Rectangle<float> VerticalRangeSlider::getUpperThumbRectangle() const
+{
+    juce::Rectangle<float> rangeRectangle = getRangeRectangle();
+
+    float thumbXPosition = rangeRectangle.getCentreX() - (thumbWidth * 0.5f);
+    float thumbYPosition = rangeRectangle.getY() + thumbTopInset;
+
+    return juce::Rectangle<float>(thumbXPosition, thumbYPosition, thumbWidth, thumbHeight);
+}
+
+juce::Rectangle<float> VerticalRangeSlider::getLowerThumbRectangle() const
+{
+    juce::Rectangle<float> rangeRectangle = getRangeRectangle();
+
+    float thumbXPosition = rangeRectangle.getCentreX() - (thumbWidth * 0.5f);
+    float thumbYPosition = rangeRectangle.getBottom() - thumbTopInset - thumbHeight;
+
+    juce::Rectangle<float> lowerThumbRectangle(thumbXPosition, thumbYPosition, thumbWidth, thumbHeight);
+    juce::Rectangle<float> upperThumbRectangle = getUpperThumbRectangle();
+
+    if (lowerThumbRectangle.getY() < upperThumbRectangle.getBottom() + visualMinimumThumbSpacing)
+    {
+        lowerThumbRectangle.setY(upperThumbRectangle.getBottom() + visualMinimumThumbSpacing);
+    }
+
+    return lowerThumbRectangle;
+}
+
+VerticalRangeSlider::HoveredThumb VerticalRangeSlider::getHoveredThumbAtPosition(juce::Point<int> mousePosition) const
+{
+    juce::Rectangle<float> upperThumbRectangle = getUpperThumbRectangle().expanded(6.0f, 6.0f);
+    juce::Rectangle<float> lowerThumbRectangle = getLowerThumbRectangle().expanded(6.0f, 6.0f);
+
+    if (upperThumbRectangle.contains(mousePosition.toFloat()))
+    {
+        return HoverUpper;
+    }
+
+    if (lowerThumbRectangle.contains(mousePosition.toFloat()))
+    {
+        return HoverLower;
+    }
+
+    return HoverNone;
 }
 
 void VerticalRangeSlider::paint(juce::Graphics& graphics)
 {
-    auto bounds = getLocalBounds().toFloat();
+    juce::Rectangle<float> bounds = getLocalBounds().toFloat();
 
-    // Draw track background (full component area)
     graphics.setColour(AccentGray);
     graphics.fillRoundedRectangle(bounds, roundness);
 
-    // Calculate range rectangle
-    float sliderWidth = bounds.getWidth();
-    float sliderX = bounds.getX() + (bounds.getWidth() - sliderWidth) / 2.0f;
-    float lowerY = valueToY(lowerValue);
-    float upperY = valueToY(upperValue);
-
-    float rangeHeight = lowerY - upperY;
-    juce::Rectangle<float> rangeRect(sliderX, upperY, sliderWidth, rangeHeight);
-
-    // Draw range rectangle
+    juce::Rectangle<float> rangeRectangle = getRangeRectangle();
     graphics.setColour(ThemePink);
-    graphics.fillRoundedRectangle(rangeRect, roundness);
+    graphics.fillRoundedRectangle(rangeRectangle, roundness);
 
-    // Draw handles (flat lines)
-    graphics.setColour(ThemePink.darker(0.2f));
-    float handleInset = handleMargin;
-    float handleX1 = rangeRect.getX() + handleInset;
-    float handleX2 = rangeRect.getRight() - handleInset;
+    juce::Colour normalThumbColour = ThemePink.darker(0.2f);
+    juce::Colour hoveredThumbColour = ThemePink.brighter(0.35f);
 
-    // Top handle (upperValue)
-    graphics.drawLine(handleX1, upperY + handleMargin, handleX2, upperY + handleMargin, (float)handleThickness);
+    juce::Rectangle<float> upperThumbRectangle = getUpperThumbRectangle();
+    juce::Rectangle<float> lowerThumbRectangle = getLowerThumbRectangle();
 
-    // Bottom handle (lowerValue)
-    graphics.drawLine(handleX1, lowerY - handleMargin, handleX2, lowerY - handleMargin, (float)handleThickness);
+    graphics.setColour(hoveredThumb == HoverUpper ? hoveredThumbColour : normalThumbColour);
+    graphics.fillRoundedRectangle(upperThumbRectangle, thumbHeight * 0.5f);
+
+    graphics.setColour(hoveredThumb == HoverLower ? hoveredThumbColour : normalThumbColour);
+    graphics.fillRoundedRectangle(lowerThumbRectangle, thumbHeight * 0.5f);
 }
 
 void VerticalRangeSlider::resized()
 {
-    // No layout needed, everything drawn relative to bounds
 }
 
 int VerticalRangeSlider::valueToY(float value) const
 {
-    auto bounds = getLocalBounds();
+    juce::Rectangle<int> bounds = getLocalBounds();
     float proportion = (value - minValue) / (maxValue - minValue);
 
-    return juce::jmap(1.0f - proportion, float(bounds.getY()), float(bounds.getBottom()));
+    return juce::roundToInt(
+        juce::jmap(1.0f - proportion, static_cast<float>(bounds.getY()), static_cast<float>(bounds.getBottom()))
+    );
 }
 
-float VerticalRangeSlider::yToValue(int y) const
+float VerticalRangeSlider::yToValue(int yPosition) const
 {
-    auto bounds = getLocalBounds();
-    float proportion = 1.0f - ((float)(y - bounds.getY()) / bounds.getHeight());
+    juce::Rectangle<int> bounds = getLocalBounds();
+    float proportion = 1.0f - (static_cast<float>(yPosition - bounds.getY()) / static_cast<float>(bounds.getHeight()));
 
     return juce::jlimit(minValue, maxValue, minValue + proportion * (maxValue - minValue));
 }
 
-void VerticalRangeSlider::mouseDown(const juce::MouseEvent& event)
+void VerticalRangeSlider::mouseDown(const juce::MouseEvent& mouseEvent)
 {
-    int mouseY = event.getPosition().getY();
-    int lowerY = valueToY(lowerValue);
-    int upperY = valueToY(upperValue);
+    hoveredThumb = getHoveredThumbAtPosition(mouseEvent.getPosition());
 
-    // Use a tolerance for the handle lines
-    int tolerance = 20;
-
-    if (std::abs(mouseY - lowerY) < tolerance)
-        dragging = Lower;
-    else if (std::abs(mouseY - upperY) < tolerance)
-        dragging = Upper;
+    if (hoveredThumb == HoverLower)
+    {
+        draggingThumb = Lower;
+    }
+    else if (hoveredThumb == HoverUpper)
+    {
+        draggingThumb = Upper;
+    }
     else
-        dragging = None;
+    {
+        draggingThumb = None;
+    }
+
+    repaint();
 }
 
-void VerticalRangeSlider::mouseDrag(const juce::MouseEvent& event)
+void VerticalRangeSlider::mouseDrag(const juce::MouseEvent& mouseEvent)
 {
-    int mouseY = event.getPosition().getY();
-    float value = yToValue(mouseY);
+    float newValue = yToValue(mouseEvent.getPosition().getY());
 
-    if (dragging == Lower)
-        setLowerValue(value);
-    else if (dragging == Upper)
-        setUpperValue(value);
+    if (draggingThumb == Lower)
+    {
+        setLowerValue(newValue);
+    }
+    else if (draggingThumb == Upper)
+    {
+        setUpperValue(newValue);
+    }
 }
 
-void VerticalRangeSlider::mouseMove(const juce::MouseEvent& event)
+void VerticalRangeSlider::mouseMove(const juce::MouseEvent& mouseEvent)
 {
-    int mouseY = event.getPosition().getY();
-    int lowerY = valueToY(lowerValue);
-    int upperY = valueToY(upperValue);
+    HoveredThumb newHoveredThumb = getHoveredThumbAtPosition(mouseEvent.getPosition());
 
-    int tolerance = 20;
+    if (hoveredThumb != newHoveredThumb)
+    {
+        hoveredThumb = newHoveredThumb;
+        repaint();
+    }
 
-    if (std::abs(mouseY - lowerY) < tolerance || std::abs(mouseY - upperY) < tolerance)
-        setMouseCursor(juce::MouseCursor::PointingHandCursor);
-    else
+    if (hoveredThumb == HoverNone)
+    {
         setMouseCursor(juce::MouseCursor::NormalCursor);
+    }
+    else
+    {
+        setMouseCursor(juce::MouseCursor::PointingHandCursor);
+    }
+}
+
+void VerticalRangeSlider::mouseExit(const juce::MouseEvent& mouseEvent)
+{
+    juce::ignoreUnused(mouseEvent);
+
+    hoveredThumb = HoverNone;
+    setMouseCursor(juce::MouseCursor::NormalCursor);
+    repaint();
+}
+
+void VerticalRangeSlider::mouseUp(const juce::MouseEvent& mouseEvent)
+{
+    juce::ignoreUnused(mouseEvent);
+
+    draggingThumb = None;
 }
--- DR-UpDownGate/Source/Utils/VerticalRangeSlider.h
+++ DR-UpDownGate/Source/Utils/VerticalRangeSlider.h
@@ -5,42 +5,76 @@
 class VerticalRangeSlider : public juce::Component
 {
 public:
-    VerticalRangeSlider(float min, float max);
+    VerticalRangeSlider(float minimumValue, float maximumValue);
     ~VerticalRangeSlider() override = default;
 
-    float getLowerValue() const { return lowerValue; }
-    float getUpperValue() const { return upperValue; }
+    float getLowerValue() const
+    {
+        return lowerValue;
+    }
+
+    float getUpperValue() const
+    {
+        return upperValue;
+    }
 
     std::function<void(float)> OnLowerValueChanged;
     std::function<void(float)> OnUpperValueChanged;
 
-    void setLowerValue(float value);
-    void setUpperValue(float value);
+    void setLowerValue(float newValue);
+    void setUpperValue(float newValue);
 
-    void setRoundness(float radius);
+    void setRoundness(float newRadius);
+    void setMinimumRange(float newMinimumRange);
 
-    void paint(juce::Graphics&) override;
+    void paint(juce::Graphics& graphics) override;
     void resized() override;
 
 protected:
-    void mouseDown(const juce::MouseEvent&) override;
-    void mouseDrag(const juce::MouseEvent&) override;
-    void mouseMove(const juce::MouseEvent&) override;
+    void mouseDown(const juce::MouseEvent& mouseEvent) override;
+    void mouseDrag(const juce::MouseEvent& mouseEvent) override;
+    void mouseMove(const juce::MouseEvent& mouseEvent) override;
+    void mouseExit(const juce::MouseEvent& mouseEvent) override;
+    void mouseUp(const juce::MouseEvent& mouseEvent) override;
 
 private:
-    float minValue, maxValue;
-    float lowerValue, upperValue;
+    enum DraggingThumb
+    {
+        None,
+        Lower,
+        Upper
+    };
 
-    float roundness = 20.0f; // default roundness
+    enum HoveredThumb
+    {
+        HoverNone,
+        HoverLower,
+        HoverUpper
+    };
 
-    enum DraggingThumb { None, Lower, Upper };
-    DraggingThumb dragging = None;
+    float minValue;
+    float maxValue;
+    float lowerValue;
+    float upperValue;
 
-    int handleThickness = 4; // thickness of the drag handles
-    int handleMargin = 8;    // margin inside the range rect for handles
+    float minimumRange = 0.25f;
+    float roundness = 20.0f;
+
+    DraggingThumb draggingThumb = None;
+    HoveredThumb hoveredThumb = HoverNone;
+
+    float thumbWidth = 18.0f;
+    float thumbHeight = 4.0f;
+    float thumbTopInset = 6.0f;
+    float visualMinimumThumbSpacing = 8.0f;
 
     int valueToY(float value) const;
-    float yToValue(int y) const;
+    float yToValue(int yPosition) const;
+
+    juce::Rectangle<float> getRangeRectangle() const;
+    juce::Rectangle<float> getUpperThumbRectangle() const;
+    juce::Rectangle<float> getLowerThumbRectangle() const;
+    HoveredThumb getHoveredThumbAtPosition(juce::Point<int> mousePosition) const;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(VerticalRangeSlider)
 };
