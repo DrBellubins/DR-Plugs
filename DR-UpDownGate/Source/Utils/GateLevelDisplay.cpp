42602545782dcd15816ce1717ffb1e38567f6b61
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2026-05-06 11:37:39 -0400

    Add gate level display

---
+++ DR-UpDownGate/Source/Utils/GateLevelDisplay.cpp
@@ -0,0 +1,5 @@
+//
+// Created by justin on 5/6/26.
+//
+
+#include "GateLevelDisplay.h"
---
+++ DR-UpDownGate/Source/Utils/GateLevelDisplay.h
@@ -0,0 +1,13 @@
+//
+// Created by justin on 5/6/26.
+//
+
+#ifndef DR_UPDOWNGATE_GATELEVELDISPLAY_H
+#define DR_UPDOWNGATE_GATELEVELDISPLAY_H
+
+
+class GateLevelDisplay {
+};
+
+
+#endif //DR_UPDOWNGATE_GATELEVELDISPLAY_H
