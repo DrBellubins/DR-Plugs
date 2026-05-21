21199c5926c08c94bc415692bfa4506a0da431cc
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2026-05-21 12:20:06 -0400

    Implement impulse click. Various variable cleanup

---
+++ DR-Chronoverb/Source/Filters/ImpulseClickSynth.h
@@ -0,0 +1,16 @@
+//
+// Created by justin on 5/21/26.
+//
+
+#ifndef CHRONOVERB_IMPULSECLICKSYNTH_H
+#define CHRONOVERB_IMPULSECLICKSYNTH_H
+
+
+
+class ImpulseClickSynth {
+
+};
+
+
+
+#endif //CHRONOVERB_IMPULSECLICKSYNTH_H
