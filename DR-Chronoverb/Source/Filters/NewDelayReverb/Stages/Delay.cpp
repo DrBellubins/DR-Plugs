754da5a8a0201fa537d0d7d798e5e5b352cd2a32
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2026-05-24 09:22:39 -0400

    Begin modularizing delay and diffusion (oh boy)

---
+++ DR-Chronoverb/Source/Filters/NewDelayReverb/Stages/Delay.cpp
@@ -0,0 +1,5 @@
+//
+// Created by justin on 5/24/26.
+//
+
+#include "Delay.h"
---
+++ DR-Chronoverb/Source/Filters/NewDelayReverb/Stages/Delay.h
@@ -0,0 +1,16 @@
+//
+// Created by justin on 5/24/26.
+//
+
+#ifndef CHRONOVERB_DELAY_H
+#define CHRONOVERB_DELAY_H
+
+
+
+class Delay {
+
+};
+
+
+
+#endif //CHRONOVERB_DELAY_H
