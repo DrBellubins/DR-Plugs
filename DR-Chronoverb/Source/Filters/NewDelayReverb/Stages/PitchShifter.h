e5f48f8e0eeb4991cc0fc6554d2b09a5c600b604
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2026-06-01 13:22:44 -0400

    Add todo

--- DR-Chronoverb/Source/Filters/NewDelayReverb/Stages/PitchShifter.h
+++ DR-Chronoverb/Source/Filters/NewDelayReverb/Stages/PitchShifter.h
@@ -8,6 +8,7 @@
 
 // TODO: Sounds very grainy at anything 24+
 // TODO: Kill myself
+// TODO: Implement deterministic jitter/randomness based on host playhead position
 // TODO: Try non-destructive improvements.
 
 class PitchShifter
